d63746b632c094c6aa67991694792b43152b8139
pekhov <pehovanton21@gmail.com>
2022-04-18 21:06:18 +0300

    com

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,41 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ProductsController
 {
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: ['POST'])]
+    public function lowestPrice(Request $request, int $id): JsonResponse
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse(
+                ['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
 
+        return new JsonResponse([
+            "quantity" => 5,
+            "request_location" => "UK",
+            "voucher_code" => "OU812",
+            "request_data" => "2022-04-04",
+            "product_id" => $id,
+            "price" => 100,
+            "discounted_price" => 50,
+            "promotion_id" => 3,
+            "promotion_name" => 'Black Friday half price sale',
+
+        ], 200);
+    }
+
+
+//    #[Route('/products/{id}/promotions', name: 'promotions', methods: ['GET'])]
+//    public function promotions()
+//    {
+//        dd('Test');
+//    }
 }
